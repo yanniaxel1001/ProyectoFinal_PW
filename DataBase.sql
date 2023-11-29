1bbf6edf6d4ec1c3ffbca6edcb79235c111cb58c
yanniaxel1001 <yanniaxelgalvanalatorre@gmail.com>
2023-11-28 22:47:20 -0600

    correcion base de datos

--- DataBase.sql
+++ DataBase.sql
@@ -1,18 +1,33 @@
 CREATE DATABASE stylehub;
 use stylehub;
 
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
 CREATE TABLE `usuarios` (
   `id` int(9) NOT NULL,
   `nombre_usuario` varchar(50) NOT NULL,
   `apellido_usuario` varchar(50) NOT NULL,
   `edad_usuario` int(3) NOT NULL,
   `email_usuario` varchar(50) NOT NULL,
-  `password_usuario` int(30) NOT NULL
+  `password_usuario` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+
+--
+-- Indices de la tabla `usuarios`
+--
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
 ALTER TABLE `usuarios`
-  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 COMMIT;

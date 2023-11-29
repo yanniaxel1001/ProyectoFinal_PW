1446846cc13f1c1fe0104488220b6d783f8b50b7
yanniaxel1001 <yanniaxelgalvanalatorre@gmail.com>
2023-11-28 20:06:52 -0600

    funcion para registrar usuario

--- registrar.php
+++ registrar.php
@@ -1,17 +1,31 @@
 <?php
     include("conexion_db.php");
 
-    $nombre = trim($_POST['nombre_usuario']);
-    $apellido = trim($_POST['apellido_usuario']);
-    $edad = trim($_POST['edad_usuario']);
-    $email = trim($_POST['email_usuario']);
-    $pass = trim($_POST['pass_usuario']);
+    if (isset($_POST['btn-guardar'])) {
+        if (strlen($_POST['nombre_usuario']) >= 1 &&
+        strlen($_POST['apellido_usuario']) >= 1 &&
+        strlen($_POST['edad_usuario']) >= 1 &&
+        strlen($_POST['email_usuario']) >= 1 &&
+        strlen($_POST['pass_usuario']) >= 1) {
+            $nombre = trim($_POST['nombre_usuario']);
+            $apellido = trim($_POST['apellido_usuario']);
+            $edad = trim($_POST['edad_usuario']);
+            $email = trim($_POST['email_usuario']);
+            $pass = trim($_POST['pass_usuario']);
 
-    $consulta = "INSERT INTO usuarios(nombre_usuario, apellido_usuario, edad_usuario, email_usuario, password_usuario)
-    VALUES ('$nombre', '$apellido', '$edad', '$email', '$pass')";
+            $consulta = "INSERT INTO usuarios(nombre_usuario, apellido_usuario, edad_usuario, email_usuario, password_usuario)
+            VALUES ('$nombre', '$apellido', '$edad', '$email', '$pass')";
 
-    mysqli_query($conexion, $consulta);
+            $resultado = mysqli_query($conexion, $consulta);
+            if ($resultado){
+                echo '
+                    <script>
+                        alert("Usuario registrado exitosamente");
+                        window.location = "user_logeado.php";
+                    </script>';
+            }
+        }    
+    }
+
     mysqli_close($conexion);
-
-    header("location: /ProyectoFinal_PW/user_logeado.php");
 ?>
